7ed24c40be7bf8738077aca073ccadca8f35db09
Artur <artur.kaminskii@o2.pl>
2025-05-15 10:48:12 +0200

    eloee

--- cwiczenie/src/ZarzadznieZoo.java
+++ cwiczenie/src/ZarzadznieZoo.java
@@ -1,15 +1,18 @@
 public class ZarzadznieZoo {
     public static void main(String[] args) {
-        Ptak ptak1 = new Ptak("ptak", 10, true);
-        Gad gad1 = new Gad("gad", 2, true);
-        Ssak ssak1 = new Ssak("ssak", 3, true);
+        Ptak ptak1 = new Ptak("orzel", 10, true);
+        Gad gad1 = new Gad("lew", 2, true);
+        Ssak ssak1 = new Ssak("krokodyl", 3, true);
 
+        ssak1.wyswietlInformacje();
         System.out.println(ssak1.wydajDzwiek());
         System.out.println(ssak1.poruszajSie());
         System.out.println(ssak1.powiedzCos());
+        gad1.wyswietlInformacje();
         System.out.println(gad1.wydajDzwiek());
         System.out.println(gad1.poruszajSie());
         System.out.println(gad1.metodaGada());
+        ptak1.wyswietlInformacje();
         System.out.println(ptak1.wydajDzwiek());
         System.out.println(ptak1.poruszajSie());
         System.out.println(ptak1.metodaPtaka());
